b23ce2bdef6ca53b51427541c8475d0841b74ed0
sundry1718 <2922956308@qq.com>
2021-07-08 10:34:11 +0800

    added new files

---
+++ data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'www.bjcs.com';
+$cfg_dbuser = 'bjcs.com';
+$cfg_dbpwd = 'bjcs.com';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
